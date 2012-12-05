d3392e0e35a0d4d9e49123278bebcbbe0904efad
Dennis Ideler <ideler.dennis@gmail.com>
2012-12-04 22:01:51 -0500

    Organize ContactInfo: put search functionality in its own function

--- HiFive/src/com/hifive/beam/ContactInfo.java
+++ HiFive/src/com/hifive/beam/ContactInfo.java
@@ -16,8 +16,6 @@ import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +27,7 @@ import android.widget.Toast;
 public class ContactInfo extends Activity {
 	
 	private static final String TAG = "ContactPicker";
+    private static TextView number;
 	
     /** Called when the activity is first created. */
     @Override
@@ -39,71 +38,74 @@ public class ContactInfo extends Activity {
         setContentView(R.layout.contact_info);
 
         // Add formatting to number field
-        final TextView number = (TextView) findViewById(R.id.number);
+        number = (TextView) findViewById(R.id.number);
         number.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+    }
+    
+    /**
+     * Searches for a contact by (given) phone number
+     * and retrieves the contact's vCard-formatted data.
+     * Called when user hits the 'Search Contacts' button.
+     * @param view
+     */
+    public void searchContact(View view)
+    {
+    	// This commented code shows or creates the contact
+    	// (if contact doesn't exist) inside the Contacts/People app.
+    	// Worst-case scenario, we beam contact straight from the open contact card.
 
-        // Handle button click by searching contacts
-        ((Button) findViewById(R.id.search)).setOnClickListener(new OnClickListener()
-        {
-        	/**
-        	 * Searches for a contact by (given) phone number
-        	 * and retrieves the contact's vCard-formatted data.
-        	 */
-            public void onClick(View view)
-            {
-            	// This commented code shows or creates the contact
-            	// (if contact doesn't exist) inside the Contacts/People app.
-            	// Worst-case scenario, we beam contact straight from the open contact card.
-
-                //Uri uri = Uri.fromParts("tel", number.getText().toString(), "");
-                //Log.d(TAG, "uri for intent: [" + uri.toString() + "]");   
-                //startActivity(new Intent(ContactsContract.Intents.SHOW_OR_CREATE_CONTACT, uri));
-            	
-            	// String phoneNumber = "289-697-1010"; // For testing
-            	String phoneNumber = number.getText().toString();
-            	if (phoneNumber.isEmpty())
-            	{
-            		toast("Please enter a phone number.\nTry your own number!");
-            		return;
-            	}
-            	String[] projection = new String[] { PhoneLookup.DISPLAY_NAME, PhoneLookup.LOOKUP_KEY };
-            	Uri lookupUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
-            	
-            	Cursor c = getContentResolver().query(lookupUri, projection, null, null, null);
-            	Log.d(TAG, DatabaseUtils.dumpCursorToString(c)); // dump contents of cursor
-            	
-            	// Verify that phone number returns info and move to the first row returned.
-            	if (c != null && c.moveToFirst())
-            	{
-            		String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)); // DISPLAY_NAME_PRIMARY will use other info if their name is not available
-            		Log.i(TAG, name);
-            		String lookupKey = c.getString(c.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
-            		c.close();
-            		
-            		// Display contact's name (or other identifying info if no name).
-            		((TextView) findViewById(R.id.contactName)).setText(name);
-            		toast("This is who you'll highfive to others!");
-            		
-            		Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_VCARD_URI, lookupKey);     
-            		
-            		try
-            		{
-	            	    AssetFileDescriptor fd = getContentResolver().openAssetFileDescriptor(uri, "r"); // TODO: is context. needed?
-	            	    FileInputStream fis = fd.createInputStream();
-	            	    byte[] b = new byte[(int) fd.getDeclaredLength()];
-	            	    fis.read(b);
-	            	    String vCard = new String(b);
-	            	    Log.d(TAG, vCard);
-            		}
-            		catch (FileNotFoundException e) { e.printStackTrace(); }
-            		catch (IOException e) { e.printStackTrace(); }
-            	}
-            	else
-            	{
-            		toast("I couldn't find a contact with that phone number. Want to try another number?");
-            	}
-            }
-        });
+        //Uri uri = Uri.fromParts("tel", number.getText().toString(), "");
+        //Log.d(TAG, "uri for intent: [" + uri.toString() + "]");   
+        //startActivity(new Intent(ContactsContract.Intents.SHOW_OR_CREATE_CONTACT, uri));
+    	
+    	// String phoneNumber = "289-697-1010"; // For testing
+    	String phoneNumber = number.getText().toString();
+    	if (phoneNumber.isEmpty())
+    	{
+    		toast("Please enter a phone number.\nTry your own number!");
+    		return;
+    	}
+    	String[] projection = new String[] { PhoneLookup.DISPLAY_NAME, PhoneLookup.LOOKUP_KEY };
+    	Uri lookupUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+    	
+    	Cursor c = getContentResolver().query(lookupUri, projection, null, null, null);
+    	Log.d(TAG, DatabaseUtils.dumpCursorToString(c)); // dump contents of cursor
+    	
+    	// Verify that phone number returns info and move to the first row returned.
+    	if (c != null && c.moveToFirst())
+    	{
+    		String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)); // DISPLAY_NAME_PRIMARY will use other info if their name is not available
+    		Log.i(TAG, name);
+    		String lookupKey = c.getString(c.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+    		c.close();
+    		
+    		// Display contact's name (or other identifying info if no name).
+    		((TextView) findViewById(R.id.contactName)).setText(name);
+    		toast("This is who you'll highfive to others!");
+    		
+    		Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_VCARD_URI, lookupKey);     
+    		
+    		try
+    		{
+        	    AssetFileDescriptor fd = getContentResolver().openAssetFileDescriptor(uri, "r"); // TODO: is context. needed?
+        	    FileInputStream fis = fd.createInputStream();
+        	    byte[] b = new byte[(int) fd.getDeclaredLength()];
+        	    fis.read(b);
+        	    String vCard = new String(b);
+        	    Log.d(TAG, vCard);
+    		}
+    		catch (FileNotFoundException e) { e.printStackTrace(); }
+    		catch (IOException e) { e.printStackTrace(); }
+    	}
+    	else
+    	{
+    		toast("I couldn't find a contact with that phone number. Want to try another number?");
+    	}
+    }
+    
+    public void setContact()
+    {
+    	// TODO
     }
     
     /**
